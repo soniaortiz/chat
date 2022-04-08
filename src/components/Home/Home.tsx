963c3cf6acf0d4cb36af2d0a9b9438bdab4722a2
Sonia <ortizt.sonia@gmail.com>
2022-04-07 21:48:55 -0700

    code cleanup

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,103 +1,90 @@
-import * as React from 'react';
-import {
-    FlatButton,
-    Paper, RefreshIndicator
-} from 'material-ui';
-import { connect } from 'react-redux';
-import { RouteComponentProps } from 'react-router';
-import { setLanguage } from '../../store/LangAction';
+import * as React from "react";
+import { FlatButton, Paper, RefreshIndicator } from "material-ui";
+import { connect } from "react-redux";
+import { RouteComponentProps } from "react-router";
+import { setLanguage } from "../../store/LangAction";
 
 class Home extends React.Component<HomeProps> {
-    componentDidMount() {
-        console.log(this.props.language);
-        this.props.loadLanguages(window.navigator.language); // set language 
-        if (!this.props.messages[this.props.language]) {
-            console.log('setting default language');
-            this.props.loadLanguages('en'); // set language 
-        }
+  componentDidMount() {
+    console.log(this.props.language);
+    this.props.loadLanguages(window.navigator.language); // set language
+    if (!this.props.messages[this.props.language]) {
+      console.log("setting default language");
+      this.props.loadLanguages("en"); // set language
     }
-    redirectToLogin = () => {
-        this.props.history.replace('/login');
-    }
+  }
+  redirectToLogin = () => {
+    this.props.history.replace("/login");
+  };
 
-    redirectToRegistration = () => {
-        this.props.history.replace('/registration');
-    }
+  redirectToRegistration = () => {
+    this.props.history.replace("/registration");
+  };
 
-    render() {
-        // console.log(this.props.messages);
-        return this.props.messages ?
-            <Paper style={{
-                position: 'fixed',
-                // backgroundImage: './523596673.jpg',
-                // background: './523596673.jpg',
-                // backgroundColor: 'pink',
-                backgroundRepeat: 'repeat-x',
-                width: '100%',
-                height: '100%',
-            }}>
-            
-                <div>
-                    <img src="./523596673.jpg" alt="fas" style={{
-                        display: 'absolute',
-                        // margin: '300px'
-                    }}/>
-                    
-                </div>
-                <FlatButton label={this.props.messages.login}
-                    onClick={this.redirectToLogin}
-                    style={{
-                        position: 'relative',
-                        top: '50%',
-                        left: '50%',
-                        marginRight: '20px',
-                        backgroundColor: '#00BCD4',
-                        color: 'white',
-                    }}
+  render() {
+    // console.log(this.props.messages);
+    return this.props.messages ? (
+      <Paper
+        style={{
+          position: "fixed",
+          // backgroundImage: './523596673.jpg',
+          // background: './523596673.jpg',
+          // backgroundColor: 'pink',
+          backgroundRepeat: "repeat-x",
+          width: "100%",
+          height: "100%"
+        }}
+      >
+        <FlatButton
+          label={this.props.messages.login}
+          onClick={this.redirectToLogin}
+          style={{
+            position: "relative",
+            top: "50%",
+            left: "50%",
+            marginRight: "20px",
+            backgroundColor: "#00BCD4",
+            color: "white"
+          }}
+        />
 
-                />
-
-                <FlatButton label={this.props.messages.signup}
-                    onClick={this.redirectToRegistration}
-                    style={{
-                        position: 'relative',
-                        top: '50%',
-                        left: '50%',
-                        marginRight: '20px',
-                        backgroundColor: '#00BCD4',
-                        color: 'white',
-                    }}
-
-                />
-            </Paper> :
-            <RefreshIndicator
-                size={40}
-                left={10}
-                top={0}
-                status="loading" />;
-    }
-
+        <FlatButton
+          label={this.props.messages.signup}
+          onClick={this.redirectToRegistration}
+          style={{
+            position: "relative",
+            top: "50%",
+            left: "50%",
+            marginRight: "20px",
+            backgroundColor: "#00BCD4",
+            color: "white"
+          }}
+        />
+      </Paper>
+    ) : (
+      <RefreshIndicator size={40} left={10} top={0} status="loading" />
+    );
+  }
 }
 
 interface HomeMS2P {
-    // tslint:disable-next-line:no-any
-    messages: any;
-    language: string;
-
+  // tslint:disable-next-line:no-any
+  messages: any;
+  language: string;
 }
 
-interface HomeOwnProps extends RouteComponentProps<{}> { }
+interface HomeOwnProps extends RouteComponentProps<{}> {}
 interface HomeD2P {
-    loadLanguages: (language: string) => void;
+  loadLanguages: (language: string) => void;
 }
 type HomeProps = HomeMS2P & HomeOwnProps & HomeD2P;
 
 export default connect<HomeMS2P, HomeD2P, HomeOwnProps, AppStore.Store>(
-    (store) => ({
-        messages: store.intlReducer.messages,
-        language: store.app.locale
-    }),
-    {
-        loadLanguages: setLanguage
-    }
+  (store) => ({
+    messages: store.intlReducer.messages,
+    language: store.app.locale
+  }),
+  {
+    loadLanguages: setLanguage
+  }
 )(Home);
--- src/store/LangAction.tsx
+++ src/store/LangAction.tsx
@@ -1,15 +1,17 @@
-import { Dispatch } from 'react-redux';
-import { updateIntl } from 'react-intl-redux';
+import { Dispatch } from "react-redux";
+import { updateIntl } from "react-intl-redux";
 
-const messages = require('../locales.json');
+const messages = require("../locales.json");
 
-export const  setLanguage = (language: string) => {
-    console.log('dispatch change of language', messages[language]); 
-    console.log('messages', messages[language]);
-    return (dispatch:  Dispatch<AppStore.App>) => {
-        return dispatch(updateIntl({
-            locale: language,
-            messages: messages[language]
-        }));
-    };
+export const setLanguage = (language: string) => {
+  console.log("dispatch change of language", messages[language]);
+  console.log("messages", messages[language]);
+  return (dispatch: Dispatch<AppStore.App>) => {
+    return dispatch(
+      updateIntl({
+        locale: language,
+        messages: messages[language]
+      })
+    );
+  };
 };
