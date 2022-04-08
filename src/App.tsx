df02b48494ec5fcbaf66a7a3d46658dc8b07021c
Sonia <ortizt.sonia@gmail.com>
2022-04-07 19:25:56 -0700

    set versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,44 @@
-import * as React from 'react';
-import * as ReactRouter from 'react-router-dom';
+import * as React from "react";
+import * as ReactRouter from "react-router-dom";
 const { BrowserRouter, Route } = ReactRouter;
-import { Signup } from './components/Signup/signup';
-import Login from './components/Login/login';
-import Dashboard from './components/Dashboard/dashboardIndex';
-import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { store as storeReducer } from './store/reducers';
-import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
-import { composeWithDevTools } from 'remote-redux-devtools';
-import { IntlProvider } from 'react-intl-redux';
-import Home from './components/Home/Home';
+import { Signup } from "./components/Signup/signup";
+import Login from "./components/Login/login";
+// import Dashboard from './components/Dashboard/dashboardIndex';
+import { MuiThemeProvider, getMuiTheme } from "material-ui/styles";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { store as storeReducer } from "./store/reducers";
+import baseTheme from "material-ui/styles/baseThemes/lightBaseTheme";
+// import { composeWithDevTools } from 'remote-redux-devtools';
+import { IntlProvider } from "react-intl-redux";
+import Home from "./components/Home/Home";
 const getTheme = () => {
   let overwrites = {};
   return getMuiTheme(baseTheme, overwrites);
 };
 
-const compeEnhancers = composeWithDevTools({ name: 'PrimitiveChat' }, );
-export const store = createStore(storeReducer, compeEnhancers(applyMiddleware(thunk)));
+// const compeEnhancers = composeWithDevTools({ name: 'PrimitiveChat' }, );
+export const store = createStore(storeReducer, applyMiddleware(thunk));
 
 class App extends React.Component {
   render() {
     return (
-      < Provider store={store}>
+      <Provider store={store}>
         <MuiThemeProvider muiTheme={getTheme()}>
-
-          <IntlProvider >
+          <IntlProvider>
             <BrowserRouter>
-
               <div>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/home" component={Home}/>
+                <Route exact path="/home" component={Home} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/registration" component={Signup} />
-                <Route exact path="/dashboard" component={Dashboard} />
+                {/* <Route exact path="/dashboard" component={Dashboard} /> */}
               </div>
-
             </BrowserRouter>
           </IntlProvider>
-
         </MuiThemeProvider>
-      </ Provider>
+      </Provider>
     );
   }
 }
